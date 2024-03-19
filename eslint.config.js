d28131ccb66ff2e393e22964635eca9b39358bac
Robbe <robbevaes95@gmail.com>
2024-03-19 15:22:47 +0100

    chore: lint fixes

--- eslint.config.js
+++ eslint.config.js
@@ -2,13 +2,4 @@ import eslintVueConfig from '@wisemen/eslint-config-vue'
 
 export default [
   ...(await eslintVueConfig),
-  {
-    rules: {
-      'function-paren-newline': [
-        'error',
-        'multiline-arguments',
-      ],
-      'putout/function-declaration-paren-newline': 'off',
-    },
-  },
 ]
