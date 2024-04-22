df4110ec7734291e0fee76fb4950c6b3a5686f45
Wouter Laermans <wouter.laermans@appwise.be>
2024-04-22 13:18:34 +0200

    improvement: keyboard shortcuts

--- src/components/app/AppNewItemButton.vue
+++ src/components/app/AppNewItemButton.vue
@@ -1,12 +1,7 @@
 <script setup lang="ts" generic="TRoute extends keyof Routes">
-import type { KeyboardKey } from '@wisemen/vue-core'
-import {
-  AppKeyboardKey,
-  AppRouterLinkButton,
-  useKeyboardCommand,
-} from '@wisemen/vue-core'
-import { ref } from 'vue'
+import { AppRouterLinkButton } from '@wisemen/vue-core'
 
+import { KEYBOARD_SHORTCUT } from '@/constants/keyboardShortcuts.constant'
 import type { Routes } from '@/routes/routes'
 import type { RouteLocationTyped } from '@/types/router/router.type'
 
@@ -14,38 +9,14 @@ const props = defineProps<{
   label: string
   to: RouteLocationTyped<TRoute>
 }>()
-
-const routerLinkButtonRef = ref<InstanceType<typeof AppRouterLinkButton> | null>(null)
-
-const keys: KeyboardKey[] = [
-  'n',
-]
-
-useKeyboardCommand({
-  command: {
-    keys,
-    onPressed: () => {
-      routerLinkButtonRef.value?.$el.click()
-    },
-    type: 'combination',
-  },
-  scope: 'global',
-})
 </script>
 
 <template>
   <AppRouterLinkButton
-    ref="routerLinkButtonRef"
     :to="props.to"
+    :keyboard-shortcut="KEYBOARD_SHORTCUT.NEW_ENTITY"
     icon-left="plus"
   >
-    <div class="flex items-center gap-x-2">
-      {{ props.label }}
-
-      <AppKeyboardKey
-        keyboard-key="n"
-        class="ml-1 bg-primary-foreground/20 !text-primary-foreground"
-      />
-    </div>
+    {{ props.label }}
   </AppRouterLinkButton>
 </template>
