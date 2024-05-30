896c51f1cabbc0b6f272789a54989168ccb511db
kirylmaltsav <kiryl.maltsav@appwise.be>
2024-05-30 16:49:09 +0200

    Update sentry sample rate env variables

--- src/plugins/sentry/sentry.plugin.ts
+++ src/plugins/sentry/sentry.plugin.ts
@@ -5,6 +5,7 @@ import {
   CURRENT_BUILD_NUMBER,
   CURRENT_ENVIRONMENT,
   SENTRY_DSN,
+  SENTRY_SAMPLE_RATE,
 } from '@/constants/environment.constant.ts'
 
 export function configureSentry(app: App<Element>): void {
@@ -22,9 +23,9 @@ export function configureSentry(app: App<Element>): void {
     dsn: SENTRY_DSN,
     environment: CURRENT_ENVIRONMENT,
     release: CURRENT_BUILD_NUMBER,
-    replaysOnErrorSampleRate: 1.0,
-    replaysSessionSampleRate: 0.1,
-    sampleRate: 1,
-    tracesSampleRate: 1,
+    replaysOnErrorSampleRate: SENTRY_SAMPLE_RATE ?? 0.1,
+    replaysSessionSampleRate: SENTRY_SAMPLE_RATE ?? 0.1,
+    sampleRate: SENTRY_SAMPLE_RATE ?? 0.1,
+    tracesSampleRate: SENTRY_SAMPLE_RATE ?? 0.1,
   })
 }
