2e72b346a6cee2c7ff947893fc362179295750b4
Wouter Laermans <wouter.laermans@appwise.be>
2024-06-02 09:58:04 +0200

    add more error translations

--- src/configs/zod.config.ts
+++ src/configs/zod.config.ts
@@ -19,7 +19,118 @@ function customErrorMap(issue: ZodIssueOptionalMessage, ctx: ErrorMapCtx): { mes
     }
   }
 
-  // TODO: Add more custom error messages here.
+  if (issue.code === z.ZodIssueCode.invalid_union) {
+    return {
+      message: t('validation.invalid_union'),
+    }
+  }
+  if (issue.code === z.ZodIssueCode.invalid_string) {
+    if (issue.validation === 'email') {
+      return {
+        message: t('validation.invalid_email'),
+      }
+    }
+    if (issue.validation === 'url') {
+      return {
+        message: t('validation.invalid_url'),
+      }
+    }
+    if (issue.validation === 'uuid') {
+      return {
+        message: t('validation.invalid_uuid'),
+      }
+    }
+    if (issue.validation === 'regex') {
+      return {
+        message: t('validation.invalid_regex'),
+      }
+    }
+    if (issue.validation === 'datetime') {
+      return {
+        message: t('validation.invalid_datetime'),
+      }
+    }
+
+    return {
+      message: t('validation.invalid_string'),
+    }
+  }
+
+  if (issue.code === z.ZodIssueCode.invalid_date) {
+    return {
+      message: t('validation.invalid_date'),
+    }
+  }
+  if (issue.code === z.ZodIssueCode.too_big) {
+    if (issue.type === 'string') {
+      return {
+        message: t('validation.too_big_string', {
+          count: issue.maximum,
+        }),
+      }
+    }
+    if (issue.type === 'number') {
+      return {
+        message: t('validation.too_big_number', {
+          count: issue.maximum,
+        }),
+      }
+    }
+    if (issue.type === 'array') {
+      return {
+        message: t('validation.too_big_array', {
+          count: issue.maximum,
+        }),
+      }
+    }
+    if (issue.type === 'date') {
+      return {
+        message: t('validation.too_big_date', {
+          count: issue.maximum,
+        }),
+      }
+    }
+
+    return {
+      message: t('validation.too_big', {
+        count: issue.maximum,
+      }),
+    }
+  }
+  if (issue.code === z.ZodIssueCode.too_small) {
+    if (issue.type === 'string') {
+      return {
+        message: t('validation.too_small_string', {
+          count: issue.minimum,
+        }),
+      }
+    }
+    if (issue.type === 'number') {
+      return {
+        message: t('validation.too_small_number', {
+          count: issue.minimum,
+        }),
+      }
+    }
+    if (issue.type === 'array') {
+      return {
+        message: t('validation.too_small_array', {
+          count: issue.minimum,
+        }),
+      }
+    }
+    if (issue.type === 'date') {
+      return {
+        message: t('validation.too_small_date', { v: issue.minimum }),
+      }
+    }
+
+    return {
+      message: t('validation.too_small', {
+        count: issue.minimum,
+      }),
+    }
+  }
 
   return {
     message: ctx.defaultError,
