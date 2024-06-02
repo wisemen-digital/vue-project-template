bb1b8e2397892c27f2fe015b27d62ef25d37075a
Wouter Laermans <wouter.laermans@appwise.be>
2024-06-02 09:26:41 +0200

    cleanup build info log

--- src/App.vue
+++ src/App.vue
@@ -7,35 +7,16 @@ import { RouterView } from 'vue-router'
 
 import AppPageLoader from '@/components/app/loader/AppPageLoader.vue'
 import ThemeProvider from '@/components/app/ThemeProvider.vue'
-import { onCreated } from '@/composables/created/created.composable.ts'
 import { useDocumentTitle } from '@/composables/document-title/documentTitle.composable'
 import { useRefreshPrompt } from '@/composables/refresh-prompt/refreshPrompt.composable'
-import {
-  CURRENT_BUILD_COMMIT,
-  CURRENT_BUILD_NUMBER,
-  CURRENT_BUILD_TIMESTAMP,
-  CURRENT_ENVIRONMENT,
-} from '@/constants/environment.constant.ts'
-import { LoggerUtil } from '@/utils/logger.util.ts'
+import { logBuildInformation } from '@/constants/environment.constant.ts'
 
 const { setTemplate } = useDocumentTitle()
 
 setTemplate('{title} | App')
 
 useRefreshPrompt()
-
-onCreated(() => {
-  if (CURRENT_BUILD_COMMIT === 'undefined') {
-    return
-  }
-
-  LoggerUtil.logInfo('Application started', {
-    COMMIT: CURRENT_BUILD_COMMIT,
-    ENVIRONMENT: CURRENT_ENVIRONMENT,
-    NUMBER: CURRENT_BUILD_NUMBER,
-    TIMESTAMP: CURRENT_BUILD_TIMESTAMP,
-  })
-})
+logBuildInformation()
 </script>
 
 <template>
--- src/constants/environment.constant.ts
+++ src/constants/environment.constant.ts
@@ -1,3 +1,6 @@
+/* eslint-disable no-console */
+import { LoggerUtil } from '@/utils/logger.util'
+
 export const API_BASE_URL = import.meta.env.API_BASE_URL
 export const API_AUTH_URL = import.meta.env.API_AUTH_URL
 export const API_CLIENT_ID = import.meta.env.API_CLIENT_ID
@@ -10,3 +13,24 @@ export const CURRENT_ENVIRONMENT = import.meta.env.ENVIRONMENT
 export const CURRENT_BUILD_NUMBER = BUILD_NUMBER
 export const CURRENT_BUILD_COMMIT = BUILD_COMMIT
 export const CURRENT_BUILD_TIMESTAMP = BUILD_TIMESTAMP
+
+export function logBuildInformation(): void {
+  if (CURRENT_BUILD_COMMIT === 'undefined') {
+    return
+  }
+
+  console.groupCollapsed('Build Information')
+
+  function logRow(label: string, value: string): void {
+    LoggerUtil.logInfo(`%c${label.padEnd(15)} %c${value}`, 'font-weight: bold;', 'font-weight: normal;')
+  }
+
+  logRow('Api Base URL', API_BASE_URL)
+  LoggerUtil.logInfo('')
+  logRow('Environment', CURRENT_ENVIRONMENT)
+  logRow('Build Number', CURRENT_BUILD_NUMBER)
+  logRow('Build Commit', CURRENT_BUILD_COMMIT)
+  logRow('Build Timestamp', CURRENT_BUILD_TIMESTAMP)
+
+  console.groupEnd()
+}
