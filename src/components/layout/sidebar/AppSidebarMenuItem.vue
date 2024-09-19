1563d438be157697c60da4407d930d6d64df4e92
Tanya-Amber-L <tanya.leenders@gmail.com>
2024-09-19 10:15:15 +0200

    use vue-core AppSidebar

--- src/components/layout/sidebar/AppSidebarMenuItem.vue
+++ src/components/layout/sidebar/AppSidebarMenuItem.vue
@@ -1,69 +1,45 @@
 <script setup lang="ts">
 import {
   AppIcon,
-  AppKeyboardShortcut,
-  AppKeyboardShortcutProvider,
   AppText,
-  AppTooltip,
 } from '@wisemen/vue-core'
-import { useI18n } from 'vue-i18n'
+import type { NavigationItemOption } from 'node_modules/@wisemen/vue-core/dist/types/navigationItem.type'
 
 import AppTypedRouterLink from '@/components/app/link/AppTypedRouterLink.vue'
-import type { NavigationItem } from '@/types/navigationItem.type'
 
 const props = defineProps<{
-  item: NavigationItem
+  isOpen: boolean
+  navigationItem: NavigationItemOption
 }>()
 
-const { t } = useI18n()
+// const { t } = useI18n()
 </script>
 
 <template>
-  <li :key="props.item.label">
-    <AppTooltip
-      :disable-close-on-trigger-click="true"
-      :hide-arrow="true"
-      side="right"
+  <div>
+    <AppTypedRouterLink
+      v-slot="{ isActive }"
+      :to="props.navigationItem.to"
+      class="block rounded-button"
     >
-      <Component
-        :is="props.item.keyboardShortcut === undefined ? 'div' : AppKeyboardShortcutProvider"
-        :config="props.item.keyboardShortcut"
+      <div
+        :class="[
+          isActive ? 'bg-primary/5 text-primary' : 'text-muted-foreground',
+          props.isOpen ? 'justify-start px-3' : 'justify-center',
+        ]"
+        class="flex h-10 w-full items-center  gap-3 rounded-button"
       >
-        <AppTypedRouterLink
-          v-slot="{ isActive }"
-          :to="props.item.to"
-          class="block rounded-button"
+        <AppIcon
+          :icon="props.navigationItem.icon"
+          size="lg"
+        />
+        <AppText
+          v-if="props.isOpen"
+          variant="subtext"
         >
-          <div
-            :class="[
-              isActive ? 'bg-primary/5 text-primary' : 'text-muted-foreground',
-            ]"
-            class="flex size-10 items-center justify-center rounded-button"
-          >
-            <AppIcon
-              :icon="props.item.icon"
-              size="lg"
-            />
-          </div>
-        </AppTypedRouterLink>
-      </Component>
-
-      <template #content>
-        <div class="flex items-center gap-x-2 px-2 py-1.5">
-          <AppText
-            variant="caption"
-            class="text-muted-foreground"
-          >
-            {{ t('components.sidebar.go_to') }} <span class="lowercase">{{ props.item.label }}</span>
-          </AppText>
-
-          <AppKeyboardShortcut
-            v-if="props.item.keyboardShortcut !== undefined"
-            :keys="props.item.keyboardShortcut.keys"
-            variant="bordered"
-          />
-        </div>
-      </template>
-    </AppTooltip>
-  </li>
+          {{ props.navigationItem.label }}
+        </AppText>
+      </div>
+    </AppTypedRouterLink>
+  </div>
 </template>
