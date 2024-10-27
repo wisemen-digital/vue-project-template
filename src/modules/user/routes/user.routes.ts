be82152c2cd5129189e2b1d3641f5e5af6490438
kirylmaltsav <kiryl.maltsav@appwise.be>
2024-10-27 13:11:43 +0100

    Remove defineAsyncComponents from routes

--- src/modules/user/routes/user.routes.ts
+++ src/modules/user/routes/user.routes.ts
@@ -1,4 +1,4 @@
-import { defineAsyncComponent } from 'vue'
+import type { Component } from 'vue'
 import type { RouteRecordRaw } from 'vue-router'
 
 export const userRoutes = [
@@ -8,23 +8,23 @@ export const userRoutes = [
       {
         name: 'user-overview',
         path: '',
-        component: defineAsyncComponent(() => import('@/modules/user/features/overview/views/UserOverviewView.vue')),
+        component: (): Component => import('@/modules/user/features/overview/views/UserOverviewView.vue'),
       },
       {
         name: 'user-detail',
         props: true,
         path: ':userUuid',
-        component: defineAsyncComponent(() => import('@/modules/user/features/detail/views/UserDetailViewDataProvider.vue')),
+        component: (): Component => import('@/modules/user/features/detail/views/UserDetailViewDataProvider.vue'),
       },
       {
         name: 'user-create',
         path: 'create',
-        component: defineAsyncComponent(() => import('@/modules/user/features/create/views/UserCreateView.vue')),
+        component: (): Component => import('@/modules/user/features/create/views/UserCreateView.vue'),
       },
       {
         name: 'user-update',
         path: ':userUuid/update',
-        component: defineAsyncComponent(() => import('@/modules/user/features/update/views/UserUpdateViewDataProvider.vue')),
+        component: (): Component => import('@/modules/user/features/update/views/UserUpdateViewDataProvider.vue'),
       },
     ],
   },
