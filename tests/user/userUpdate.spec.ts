c8fb93fe12316ca9be50503e8428cc2f86e130a6
Tanya-Amber-L <tanya.leenders@gmail.com>
2025-01-30 12:00:46 +0100

    Uncomment tests + fix a11y report issues

--- tests/user/userUpdate.spec.ts
+++ tests/user/userUpdate.spec.ts
@@ -1,39 +1,44 @@
+import { TEST_ID } from '@/constants/testId.constant'
+import { UserDtoBuilder } from '@/models/user/detail/userDto.builder'
+import { UserIndexDtoBuilder } from '@/models/user/index/userIndexDto.builder'
+import type { UserUuid } from '@/models/user/userUuid.model'
+import { UuidUtil } from '@/utils/uuid.util'
 import { expect, test } from '@@/base.fixture'
+import { InterceptorUtil } from '@@/utils/interceptor.util'
 
 test.describe('User Update', () => {
-  test('update an existing user', () => {
-    expect(true).toBe(true)
-    // const USER_UUID_1 = UuidUtil.getRandom<UserUuid>()
+  test('update an existing user', async ({ page }) => {
+    const USER_UUID_1 = UuidUtil.getRandom<UserUuid>()
 
-    // const USER_INDEX_1 = new UserIndexDtoBuilder()
-    //   .withUuid(USER_UUID_1)
-    //   .withFirstName('John')
-    //   .withLastName('Doe')
-    //   .build()
+    const USER_INDEX_1 = new UserIndexDtoBuilder()
+      .withUuid(USER_UUID_1)
+      .withFirstName('John')
+      .withLastName('Doe')
+      .build()
 
-    // const USER_1 = new UserDtoBuilder()
-    //   .withUuid(USER_UUID_1)
-    //   .build()
+    const USER_1 = new UserDtoBuilder()
+      .withUuid(USER_UUID_1)
+      .build()
 
-    // await InterceptorUtil.getPaginated(page, 'users?limit=20&offset=0', [
-    //   USER_INDEX_1,
-    // ])
+    await InterceptorUtil.getPaginated(page, 'users?limit=20&offset=0', [
+      USER_INDEX_1,
+    ])
 
-    // await InterceptorUtil.get(page, `users/${USER_UUID_1}`, USER_1)
+    await InterceptorUtil.get(page, `users/${USER_UUID_1}`, USER_1)
 
-    // const updateRequest = await InterceptorUtil.post(page, `users/${USER_UUID_1}`, USER_1)
+    const updateRequest = await InterceptorUtil.post(page, `users/${USER_UUID_1}`, USER_1)
 
-    // await page.goto('/users')
+    await page.goto(`/users/${USER_UUID_1}`)
 
-    // await expect(page.getByTestId(TEST_ID.APP_PAGE.TITLE)).toHaveText('John Doe')
+    await expect(page.getByTestId(TEST_ID.APP_PAGE.TITLE)).toHaveText('John Doe')
 
-    // await page.getByTestId(TEST_ID.USERS.DETAIL.EDIT_BUTTON).click()
+    await page.getByTestId(TEST_ID.USERS.DETAIL.EDIT_BUTTON).click()
 
-    // await page.getByTestId(TEST_ID.USERS.FORM.FIRST_NAME_INPUT).fill('James')
-    // await page.getByTestId(TEST_ID.USERS.FORM.LAST_NAME_INPUT).fill('Dyson')
-    // await page.getByTestId(TEST_ID.USERS.FORM.SUBMIT_BUTTON).click()
+    await page.getByTestId(TEST_ID.USERS.FORM.FIRST_NAME_INPUT).fill('James')
+    await page.getByTestId(TEST_ID.USERS.FORM.LAST_NAME_INPUT).fill('Dyson')
+    await page.getByTestId(TEST_ID.USERS.FORM.SUBMIT_BUTTON).click()
 
-    // await expect(page.getByTestId(TEST_ID.USERS.UPDATE.SUCCESS_TOAST)).toBeVisible()
-    // expect(updateRequest.getCount()).toBe(1)
+    await expect(page.getByTestId(TEST_ID.USERS.UPDATE.SUCCESS_TOAST)).toBeVisible()
+    expect(updateRequest.getCount()).toBe(1)
   })
 })
