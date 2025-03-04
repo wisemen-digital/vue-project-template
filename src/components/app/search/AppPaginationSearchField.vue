5950b4978a41e742648e553b4e8045c18ebe38d0
Kiryl Maltsav <kiryl.maltsav@wisemen.digital>
2025-03-04 13:28:13 +0100

    Move query serializer to setupConfig

--- src/components/app/search/AppPaginationSearchField.vue
+++ src/components/app/search/AppPaginationSearchField.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts" generic="T">
-import type { Pagination } from '@wisemen/vue-core'
-import { computed } from 'vue'
+import {
+  type Pagination,
+  useDebounceSearch,
+} from '@wisemen/vue-core'
 
 import AppSearchInputField from '@/components/app/search/AppSearchInputField.vue'
 
@@ -16,18 +18,9 @@ const props = withDefaults(defineProps<{
   placeholder: null,
 })
 
-let timeout: ReturnType<typeof setTimeout> | null = null
-
-const search = computed<string>({
-  get: () => props.pagination.paginationOptions.value.search ?? '',
-  set: (value) => {
-    if (timeout !== null) {
-      clearTimeout(timeout)
-    }
-
-    timeout = setTimeout(() => {
-      props.pagination.handleSearchChange(value)
-    }, 300)
+const { search } = useDebounceSearch({
+  onDebounceSearch: (value) => {
+    props.pagination.handleSearchChange(value)
   },
 })
 </script>
