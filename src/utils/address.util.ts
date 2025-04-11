564c9dfc7a4b07397f396106ddd08baacbae7f2b
Jeroen Van Caekenberghe <jeroen.vancaekenberghe@appwise.be>
2025-04-11 13:02:23 +0200

    fix: pr remarks

--- src/utils/address.util.ts
+++ src/utils/address.util.ts
@@ -2,6 +2,10 @@ import type { Address } from '@/models/address/address.model.ts'
 
 export class AddressUtil {
   static format(address: Address | null): string {
-    return address ? `${address.streetName ?? ''} ${address.streetNumber ?? ''}, ${address.city ?? ''}, ${address.postalCode ?? ''}, ${address.country ?? ''}` : '-'
+    if (address === null) {
+      return '-'
+    }
+
+    return `${address.streetName ?? ''} ${address.streetNumber ?? ''}, ${address.city ?? ''}, ${address.postalCode ?? ''}, ${address.country ?? ''}`
   }
 }
